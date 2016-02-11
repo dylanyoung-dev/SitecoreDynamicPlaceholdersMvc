7b306a046572e609eafb3039e77103a151b35928
Dylan Young <dylan.young@arrowdesigns.com>
2016-02-10 23:59:05 -0500

    Initial Checkin

---
+++ Site.Web/Helpers/SitecoreHelperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Mvc.Helpers;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Site.Web.Helpers
+{
+    public static class SitecoreHelperExtensions
+    {
+        public static IHtmlString DynamicPlaceholder(this SitecoreHelper sitecoreHelper, string placeholderName)
+        {
+            Rendering currentRendering = RenderingContext.Current.Rendering;
+            return sitecoreHelper.Placeholder(string.Format("{0}|{1}", placeholderName, currentRendering.UniqueId.ToString()));
+        }
+    }
+}
