e3e9a3f09c01a2b5a3b938c8a96d0b31bc02b2e1
aiswebmaster <dylan@allisolutions.com>
2017-05-27 21:26:41 -0400

    Re-Architecture of DynamicPlaceholders to use Helix Best Practices + Removed Test Project

---
+++ Foundation/DynamicPlaceholders/Sitecore.Foundation.DynamicPlaceholders/Helpers/SitecoreHelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Mvc.Helpers;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Foundation.DynamicPlaceholders.Helpers
+{
+    public static class SitecoreHelperExtensions
+    {
+        public static IHtmlString DynamicPlaceholders(this SitecoreHelper sitecoreHelper, string placeholderName)
+        {
+            Rendering currentRendering = RenderingContext.Current.Rendering;
+
+            return sitecoreHelper.Placeholder(string.Format("{0}|{1}", placeholderName, currentRendering.UniqueId.ToString()));
+        }
+    }
+}
